1f56a64b9515c7092a6586015b47dba4821df001
Platae <platae93@gmail.com>
2020-01-03 10:53:47 -0600

    Endpoint base para entrada de nuevos
    -Se añadiño metodo get
    -Se devuelve objeto creado

---
+++ new-entries/index.js
@@ -0,0 +1,55 @@
+module.exports = function (context, req) {
+
+    //Create entry
+    if (req.method === "POST") {
+        var entry = context.bindings.entry;
+        if (entry) {
+            context.res = {
+                status: 422,
+                body: "Entry already exists.",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+        }
+        else {
+            var date = new Date();
+            var date_string = date.toISOString();
+            // Create a JSON string.
+            var entryString = JSON.stringify({
+                id: req.body.id,
+                fecha_hora: date_string,
+                tipo_entrada: "Nuevos"
+            });
+
+            // Write the entry to the database.
+            context.bindings.newEntry = entryString;
+
+            // Push this bookmark onto our queue for further processing.
+            //context.bindings.newmessage = bookmarkString;
+
+            // Tell the user all is well.
+            context.res = {
+                status: 200,
+                body: entryString,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+        }
+        context.done();
+    }
+
+    //Get entries
+    if(req.method==="GET"){
+        var entries = context.bindings.entries;
+        context.res = {
+            status: 200,
+            body: entries,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        context.done();
+    }
+};
