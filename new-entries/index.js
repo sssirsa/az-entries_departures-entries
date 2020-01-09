b293ead29bb22557c9d8f949110ef95b5a0d17ce
platae93-sssirsa <59069673+platae93-sssirsa@users.noreply.github.com>
2020-01-09 11:29:29 -0600

    Se actualizó el get de entrada de nuevos

--- new-entries/index.js
+++ new-entries/index.js
@@ -1,18 +1,13 @@
-module.exports = function (context, req) {
+const mongodb = require('mongodb');
+//db connections
+let mongo_client= null;
+let cosmos_client = null;
+const connection_mongoDB = process.env["connection_mongoDB"];
+const connection_cosmosDB = process.env["connection_cosmosDB"];
 
+module.exports = function (context, req) {
     //Create entry
     if (req.method === "POST") {
-        var entry = context.bindings.entry;
-        if (entry) {
-            context.res = {
-                status: 422,
-                body: "Entry already exists.",
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            };
-        }
-        else {
             var date = new Date();
             var date_string = date.toISOString();
             // Create a JSON string.
@@ -25,10 +20,6 @@ module.exports = function (context, req) {
             // Write the entry to the database.
             context.bindings.newEntry = entryString;
 
-            // Push this bookmark onto our queue for further processing.
-            //context.bindings.newmessage = bookmarkString;
-
-            // Tell the user all is well.
             context.res = {
                 status: 200,
                 body: entryString,
@@ -36,7 +27,7 @@ module.exports = function (context, req) {
                     'Content-Type': 'application/json'
                 }
             };
-        }
+        
         context.done();
     }
 
@@ -48,25 +39,110 @@ module.exports = function (context, req) {
             requestedID = req.query["id"];
         }
         if (requestedID) {
-            var entry = context.bindings.entry;
-            context.res = {
-                status: 200,
-                body: entry,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            };
+            //Get specific entry
+             createCosmosClient()
+                .then(function () {
+                    getEntry(requestedID)
+                        .then(function (entry) {
+                            context.res = {
+                                status: 200,
+                                body: entry,
+                                headers:{
+                                    'Content-Type':'application/json'
+                                }
+                            };
+                            context.done();
+                        })
+                        .catch(function (error) {
+                            context.log('Error reading entry from database');
+                            context.log(error);
+                            context.res = { status: 500, body: error };
+                            context.done();
+                        });
+                })
+                .catch(function (error) {
+                    context.log('Error creating cosmos_client for entry detail');
+                    context.log(error);
+                    context.res = { status: 500, body: error };
+                    context.done();
+                });
         }
         else {
-            var entries = context.bindings.entries;
-            context.res = {
-                status: 200,
-                body: entries,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            };
+            //Get entries list
+            createCosmosClient()
+                .then(function () {
+                    getEntries()
+                        .then(function (entriesList) {
+                            context.res = {
+                                body: entriesList,
+                                headers:{
+                                    'Content-Type':'application/json'
+                                }
+                            };
+                            context.done();
+                        })
+                        .catch(function (error) {
+                            context.log('Error entries list from database');
+                            context.log(error);
+                            context.res = { status: 500, body: error };
+                            context.done();
+                        });
+                })
+                .catch(function (error) {
+                    context.log('Error creating cosmos_client for entries list');
+                    context.log(error);
+                    context.res = { status: 500, body: error };
+                    context.done();
+                });
         }
         context.done();
     }
+    
+    function createCosmosClient() {
+        return new Promise(function (resolve, reject) {
+            if (!cosmos_client) {
+                mongodb.MongoClient.connect(connection_cosmosDB, function (error, _cosmos_client) {
+                    if (error) {
+                        reject(error);
+                    }
+                    cosmos_client = _cosmos_client;
+                    resolve();
+                });
+            }
+            else {
+                resolve();
+            }
+        });
+    }    
+
+    function getEntry(entryId) {
+        return new Promise(function (resolve, reject) {
+            cosmos_client
+                .db('EntriesDepartures')
+                .collection('Entries')
+                .findOne({ _id: mongodb.ObjectId(entryId) },
+                    function (error, docs) {
+                        if (error) {
+                            reject(error);
+                        }
+                        resolve(docs);
+                    }
+                );
+        });
+    }
+
+    function getEntries(query) {
+        return new Promise(function (resolve, reject) {
+            cosmos_client
+                .db('EntriesDepartures')
+                .collection('Entries')
+                .find(query)
+                .toArray(function (error, docs) {
+                    if (error) {
+                        reject(error);
+                    }
+                    resolve(docs)
+                });
+        });
+    }    
 };
