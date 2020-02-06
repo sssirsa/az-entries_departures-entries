a71e13a59f5e3ae9ccd08ae31b49061c81fb3140
Platae <platae93@gmail.com>
2020-02-06 17:20:23 -0600

    Corrección de campos en entrada de buen estado

--- warehouse-entries/index.js
+++ warehouse-entries/index.js
@@ -150,12 +150,12 @@ module.exports = function (context, req) {
         //TODO: Get person data trough userid and save it in the entry data
         let entry; //Base object
         var userId = null;
-        var destinationAgencyId = req.body['udn_destino_id'];
-        var destinationSubsidiaryId = req.body['sucursal_destino_id'];
-        var originAgencyId = req.body['udn_origen_id'];
-        var originSubsidiaryId = req.body['sucursal_origen_id'];
-        var transportDriverId = req.body['operador_transporte_id'];
-        var transportKindId = req.body['tipo_transporte_id']; //Non mandatory
+        var destinationAgencyId = req.body['udn_destino'];
+        var destinationSubsidiaryId = req.body['sucursal_destino'];
+        var originAgencyId = req.body['udn_origen'];
+        var originSubsidiaryId = req.body['sucursal_origen'];
+        var transportDriverId = req.body['operador_transporte'];
+        var transportKindId = req.body['tipo_transporte']; //Non mandatory
 
         validate();
 
@@ -184,7 +184,7 @@ module.exports = function (context, req) {
             if (transportKindId) {
                 transportKind = await searchTransportKind(transportKindId);
             }
-            let fridges = await searchAllFridges(req.body['cabinets_id']);
+            let fridges = await searchAllFridges(req.body['cabinets']);
 
             let precedentPromises = [originAgency, originSubsidiary, destinationAgency, destinationSubsidiary, transportDriver, transportKind, fridges];
 
@@ -282,7 +282,7 @@ module.exports = function (context, req) {
             }
 
             //Fridge array validation
-            if (!req.body.cabinets_id) {
+            if (!req.body.cabinets) {
                 //No array
                 context.res = {
                     status: 400,
@@ -295,7 +295,7 @@ module.exports = function (context, req) {
                 };
                 context.done();
             }
-            if (req.body.cabinets_id.length === 0) {
+            if (req.body.cabinets.length === 0) {
                 //Empty array
                 context.res = {
                     status: 400,
